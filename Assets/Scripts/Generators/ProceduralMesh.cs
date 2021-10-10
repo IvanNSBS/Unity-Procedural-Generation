6d21415ea192857ecaa82a25cfefe3c3652236f6
Ivan Neves <insbs@cin.ufpe.br>
2021-10-10 19:58:21 -0300

    Adding basic chunk generation

---
+++ Assets/Scripts/Generators/ProceduralMesh.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Generators
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public abstract class ProceduralMesh : MonoBehaviour
+    {
+        #region Fields
+        protected Vector3[] vertices;
+        protected int[] indices;
+        #endregion Fields
+        
+        public abstract void DeleteMesh();
+        public abstract void GenerateMesh(Texture2D heightmap);
+    }
+}
---
+++ Assets/Scripts/Generators/VoxelChunkGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Generators
+{
+    public class VoxelChunkGenerator : ProceduralMesh
+    {
+        #region Constants
+        private const int CHUNK_WIDTH = 16;
+        private const int CHUNK_HEIGHT = 256;
+        #endregion Constantss
+        
+        public override void DeleteMesh()
+        {
+        }
+
+        public override void GenerateMesh(Texture2D heightmap)
+        {
+            List<Vector3> allVerts = new List<Vector3>();
+            List<int> allIdxs = new List<int>();
+            
+            for (int x = 0; x < CHUNK_WIDTH; x++)
+            {
+                for (int y = 0; y < CHUNK_WIDTH; y++)
+                {
+                    int idx = y * CHUNK_WIDTH + x;
+                    var tuple = GenerateVoxel(8*idx, x, 0, y);
+                    
+                    allVerts.AddRange(tuple.Item1);
+                    allIdxs.AddRange(tuple.Item2);
+                }
+            }
+
+            var mesh = GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
+            
+            // // TODO: Use more efficient method here
+            mesh.vertices = vertices = allVerts.ToArray();
+            mesh.triangles = indices = allIdxs.ToArray();
+            Debug.Log($"Vertices Size: {vertices.Length}");
+            mesh.Optimize();
+        }
+
+        private void Start()
+        {
+            GenerateMesh(new Texture2D(16, 16));
+        }
+
+
+        #region Helper Methods
+        private Tuple<Vector3[], int[]> GenerateVoxel(int idx, int posX, int posY, int posZ)
+        {
+            Vector3[] verts = {
+                new Vector3 (0 + posX, 0 + posY, 0 + posZ),
+                new Vector3 (0 + posX, 0 + posY, 1 + posZ),
+                new Vector3 (1 + posX, 0 + posY, 1 + posZ),
+                new Vector3 (1 + posX, 0 + posY, 0 + posZ),
+                
+                new Vector3 (0 + posX, 1 + posY, 0 + posZ),
+                new Vector3 (0 + posX, 1 + posY, 1 + posZ),
+                new Vector3 (1 + posX, 1 + posY, 1 + posZ),
+                new Vector3 (1 + posX, 1 + posY, 0 + posZ),
+            };
+            
+            // Vector3[] normals = {
+            //     new Vector3 (0, 1, 0), // up
+            //     new Vector3 (0, -1,0), // bottom
+            //     new Vector3 (-1, 0, 0), // left
+            //     new Vector3 (1, 0, 0), // right
+            //     
+            //     new Vector3 (0, -1, 0), // front
+            //     new Vector3 (0, 1, 0) // back
+            // };
+
+            int[] idxs = {
+                idx + 2, idx + 1, idx + 0, // bottom face
+                idx + 0, idx + 3, idx + 2,
+                
+                idx + 4, idx + 5, idx + 6, // top face
+                idx + 6, idx + 7, idx + 4,
+                
+                idx + 4, idx + 0, idx + 1, // side left face
+                idx + 1, idx + 5, idx + 4,
+                
+                idx + 2, idx + 3, idx + 7, // side right face
+                idx + 7, idx + 6, idx + 2,
+                
+                idx + 0, idx + 4, idx + 7, // front face
+                idx + 7, idx + 3, idx + 0,
+                
+                idx + 6, idx + 5, idx + 1, // back face
+                idx + 1, idx + 2, idx + 6
+            };
+ 
+            // Vector2[] texCoords =
+            // {
+            //     new Vector2(0, 0),
+            //     new Vector2(1, 0),
+            //     new Vector2(1, 1),
+            //     new Vector2(0, 1)
+            // };
+            //
+            // int[] texIdxs = { 0, 1, 3, 3, 1, 2 };
+            return new Tuple<Vector3[], int[]>(verts, idxs);
+        }
+        #endregion Helper Methods
+    }
+}

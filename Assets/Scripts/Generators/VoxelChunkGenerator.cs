5b28ed3bf9b71dde49b83310ffaa7b4af5cf2616
Ivan Neves <insbs@cin.ufpe.br>
2021-10-11 10:20:55 -0300

    Increasing VoxelChunkGenerator performance by using inplace arrays

--- Assets/Scripts/Generators/VoxelChunkGenerator.cs
+++ Assets/Scripts/Generators/VoxelChunkGenerator.cs
@@ -18,95 +18,72 @@ namespace Generators
 
         public override void GenerateMesh(Texture2D heightmap, int chunkX, int chunkY, NoiseData data)
         {
-            List<Vector3> allVerts = new List<Vector3>();
-            List<int> allIdxs = new List<int>();
-            
+            vertices = new Vector3[8*CHUNK_SIZE*CHUNK_SIZE];
+            indices = new int[36*CHUNK_SIZE*CHUNK_SIZE];
             data.SetNoise();
             
             for (int x = 0; x < CHUNK_SIZE; x++)
             {
                 for (int y = 0; y < CHUNK_SIZE; y++)
                 {
-                    int idx = y * CHUNK_SIZE + x;
+                    int index = y * CHUNK_SIZE + x;
+                    int verticesStride = index * 8;
+                    int indicesStride = index * 36;
                     int actualX = x + chunkX * CHUNK_SIZE;
                     int actualY = y + chunkY * CHUNK_SIZE;
                     
                     float normalizedNoise = (data.noise.GetSimplexFractal(actualX, actualY) + 1) / 2f;
                     int height = (int)Math.Floor(normalizedNoise * CHUNK_HEIGHT);
                     
-                    var tuple = GenerateVoxel(8*idx, x, height, y);
-                    
-                    allVerts.AddRange(tuple.Item1);
-                    allIdxs.AddRange(tuple.Item2);
+                    GenerateVoxel(verticesStride, indicesStride, x, height, y);
                 }
             }
 
             var mesh = GetComponent<MeshFilter>().mesh;
             mesh.Clear();
             
-            // // TODO: Use more efficient method here
-            mesh.vertices = vertices = allVerts.ToArray();
-            mesh.triangles = indices = allIdxs.ToArray();
+            mesh.vertices = vertices;
+            mesh.triangles = indices;
             mesh.Optimize();
 
             transform.position = new Vector3(chunkX * CHUNK_SIZE, 0, chunkY * CHUNK_SIZE);
         }
 
         #region Helper Methods
-        private Tuple<Vector3[], int[]> GenerateVoxel(int idx, int posX, int posY, int posZ)
+        private void GenerateVoxel(int verticesStride, int indicesStride, int posX, int posY, int posZ)
         {
-            Vector3[] verts = {
-                new Vector3 (0 + posX, 0 + posY, 0 + posZ),
-                new Vector3 (0 + posX, 0 + posY, 1 + posZ),
-                new Vector3 (1 + posX, 0 + posY, 1 + posZ),
-                new Vector3 (1 + posX, 0 + posY, 0 + posZ),
-                
-                new Vector3 (0 + posX, 1 + posY, 0 + posZ),
-                new Vector3 (0 + posX, 1 + posY, 1 + posZ),
-                new Vector3 (1 + posX, 1 + posY, 1 + posZ),
-                new Vector3 (1 + posX, 1 + posY, 0 + posZ),
-            };
+            vertices[verticesStride]   = new Vector3 (0 + posX, 0 + posY, 0 + posZ);
+            vertices[verticesStride+1] = new Vector3 (0 + posX, 0 + posY, 1 + posZ);
+            vertices[verticesStride+2] = new Vector3 (1 + posX, 0 + posY, 1 + posZ);
+            vertices[verticesStride+3] = new Vector3 (1 + posX, 0 + posY, 0 + posZ);
+                            
+            vertices[verticesStride+4] = new Vector3 (0 + posX, 1 + posY, 0 + posZ);
+            vertices[verticesStride+5] = new Vector3 (0 + posX, 1 + posY, 1 + posZ);
+            vertices[verticesStride+6] = new Vector3 (1 + posX, 1 + posY, 1 + posZ);
+            vertices[verticesStride+7] = new Vector3 (1 + posX, 1 + posY, 0 + posZ);
             
-            // Vector3[] normals = {
-            //     new Vector3 (0, 1, 0), // up
-            //     new Vector3 (0, -1,0), // bottom
-            //     new Vector3 (-1, 0, 0), // left
-            //     new Vector3 (1, 0, 0), // right
-            //     
-            //     new Vector3 (0, -1, 0), // front
-            //     new Vector3 (0, 1, 0) // back
-            // };
-
             int[] idxs = {
-                idx + 2, idx + 1, idx + 0, // bottom face
-                idx + 0, idx + 3, idx + 2,
+                verticesStride + 2, verticesStride + 1, verticesStride + 0, // bottom face
+                verticesStride + 0, verticesStride + 3, verticesStride + 2,
                 
-                idx + 4, idx + 5, idx + 6, // top face
-                idx + 6, idx + 7, idx + 4,
+                verticesStride + 4, verticesStride + 5, verticesStride + 6, // top face
+                verticesStride + 6, verticesStride + 7, verticesStride + 4,
                 
-                idx + 4, idx + 0, idx + 1, // side left face
-                idx + 1, idx + 5, idx + 4,
+                verticesStride + 4, verticesStride + 0, verticesStride + 1, // side left face
+                verticesStride + 1, verticesStride + 5, verticesStride + 4,
                 
-                idx + 2, idx + 3, idx + 7, // side right face
-                idx + 7, idx + 6, idx + 2,
+                verticesStride + 2, verticesStride + 3, verticesStride + 7, // side right face
+                verticesStride + 7, verticesStride + 6, verticesStride + 2,
                 
-                idx + 0, idx + 4, idx + 7, // front face
-                idx + 7, idx + 3, idx + 0,
+                verticesStride + 0, verticesStride + 4, verticesStride + 7, // front face
+                verticesStride + 7, verticesStride + 3, verticesStride + 0,
                 
-                idx + 6, idx + 5, idx + 1, // back face
-                idx + 1, idx + 2, idx + 6
+                verticesStride + 6, verticesStride + 5, verticesStride + 1, // back face
+                verticesStride + 1, verticesStride + 2, verticesStride + 6
             };
- 
-            // Vector2[] texCoords =
-            // {
-            //     new Vector2(0, 0),
-            //     new Vector2(1, 0),
-            //     new Vector2(1, 1),
-            //     new Vector2(0, 1)
-            // };
-            //
-            // int[] texIdxs = { 0, 1, 3, 3, 1, 2 };
-            return new Tuple<Vector3[], int[]>(verts, idxs);
+
+            for (int i = 0; i < idxs.Length; i++)
+                indices[indicesStride + i] = idxs[i];
         }
         #endregion Helper Methods
     }

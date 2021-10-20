c196ab6aff705991aabd651ef3e3f1c57ef66fd1
Ivan Neves <insbs@cin.ufpe.br>
2021-10-20 20:08:38 -0300

    TextureDisplay now displays HeightmapGenerator heightmap

--- Assets/Scripts/Utils/TextureDisplay.cs
+++ Assets/Scripts/Utils/TextureDisplay.cs
@@ -1,5 +1,5 @@
 ﻿using Data;
-using DataScriptable;
+using Generators;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,12 +11,11 @@ namespace Utils
         [SerializeField] private RawImage m_imageRef;
         [SerializeField] private Vector2Int m_imageSize;
         [Header("Noise Data")] 
-        [SerializeField] private BiomeMap m_biomeData;
-        [SerializeField] private NoiseData[] m_noiseData;
+        [SerializeField] private HeightmapGenerator m_generator;
+        [SerializeField] [Range(0, 1)] private float m_waterLevel;
         #endregion Inspector Fields
 
         #region Fields
-        private Vector2Int m_position;
         private Texture2D m_texture;
         #endregion Fields
         
@@ -26,7 +25,6 @@ namespace Utils
         {
             m_texture = new Texture2D(m_imageSize.x, m_imageSize.y);
             m_texture.filterMode = FilterMode.Point;
-            m_position = new Vector2Int(0, 0);
             m_imageRef.texture = m_texture;
             
             UpdateTextures();
@@ -46,26 +44,12 @@ namespace Utils
             {
                 for (int y = 0; y < m_imageSize.y; y++)
                 {
-                    float p = 0;
-
-                    var noiseData = x > m_imageSize.x/2 ? m_noiseData[0] : m_noiseData[1];
-                    // foreach (var noiseData in m_noiseData)
-                    // {
-                        noiseData.SetNoise();
-                        float noise = (noiseData.noise.GetSimplexFractal(x + m_position.x, y + m_position.y) + 1) / 2f;
-                        noise *= noiseData.maxHeight;
-                        p += noise;
-                    // }
-                    
+                    float p = m_generator.GetHeightAt(x, y);
 
                     if (p > 1)
                         p = 1;
                     
-                    var baseCol = new Color(0.05f, p*2, 0.02f);
-                    if (p < 0.15f)
-                        baseCol = new Color(0.05f, 0.02f, p*2);
-                    if (p > 0.88f)
-                        baseCol = new Color(0.8f*p, 0.8f*p, 0.8f*p*2);
+                    var baseCol = p < m_waterLevel ?  new Color(0.05f, p, 0.02f) : new Color(0.05f, 0.02f, p);
                     m_texture.SetPixel(x, y, baseCol);
                 }
             }
